7f1c7fec05f8ce825ee17fc424a84e6a4c659444
southerncross <southerncross@pku.edu.cn>
2014-03-26 10:38:50 +0800

    Update JitMeetConnector.java
    Add packets listener, so we can now capture sent or incoming packets. But there are still some packets can't be captured, it's a problem.

--- JitMeetConnector.java
+++ JitMeetConnector.java
@@ -2,42 +2,101 @@ package test;
 
 import java.util.Scanner;
 
+import net.java.sip.communicator.impl.protocol.jabber.extensions.jingle.JingleIQ;
+
 import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.PacketListener;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.filter.PacketFilter;
+import org.jivesoftware.smack.packet.Packet;
 import org.jivesoftware.smackx.muc.MultiUserChat;
 
 public class JitMeetConnector
 {
     public static void main(String[] args)
     {
-        ConnectionConfiguration conf = new ConnectionConfiguration("jitmeet.example.com", 5222);
+        // create XMPP connection
+        ConnectionConfiguration conf =
+            new ConnectionConfiguration("jitmeet.example.com", 5222);
         XMPPConnection.DEBUG_ENABLED = true;
         XMPPConnection connection = new XMPPConnection(conf);
-        
+
         try
         {
+            // login anonymously, get temporary account
             connection.connect();
             connection.loginAnonymously();
-            
+
+            // display connection information
             System.out.println("ConnectionID: " + connection.getConnectionID());
             System.out.println("Host: " + connection.getHost());
             System.out.println("Port: " + connection.getPort());
             System.out.println("ServiceName: " + connection.getServiceName());
             System.out.println("User: " + connection.getUser());
-            
-            MultiUserChat muc = new MultiUserChat(connection, "0kn6mlqar3w8w7b9@conference.jitmeet.example.com");
+
+            // display all sent packets
+            connection.addPacketSendingListener(new PacketListener()
+            {
+
+                @Override
+                public void processPacket(Packet packet)
+                {
+                    System.out.println("----->: " + packet.toXML());
+                }
+            }, new PacketFilter()
+            {
+
+                @Override
+                public boolean accept(Packet packet)
+                {
+                    return true;
+                }
+            });
+
+            // display all incoming packets
+            connection.addPacketListener(new PacketListener()
+            {
+                @Override
+                public void processPacket(Packet packet)
+                {
+                    if (packet.getClass() == JingleIQ.class)
+                    {
+                        JingleIQ j = (JingleIQ) packet;
+                        System.out.println("<-----: [jingle packet] "
+                            + j.getSID() + " : " + j.getAction() + " : "
+                            + packet.toXML());
+                    }
+                    else
+                    {
+                        System.out.println("<-----: " + packet.toXML());
+                    }
+                }
+            }, new PacketFilter()
+            {
+
+                @Override
+                public boolean accept(Packet packet)
+                {
+                    return true;
+                }
+            });
+
+            // join conference
+            MultiUserChat muc =
+                new MultiUserChat(connection,
+                    "krephqivqz7iudi@conference.example.com");
             muc.join("JitMeetConnector");
-            
+
+            Scanner s = new Scanner(System.in);
             while (true)
             {
-                Scanner s = new Scanner(System.in);
                 String str = s.nextLine();
                 if (str.equalsIgnoreCase("bye"))
                     break;
                 muc.sendMessage(str);
             }
-            
+
             connection.disconnect();
         }
         catch (XMPPException e)
